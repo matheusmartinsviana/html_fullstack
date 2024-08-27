d6a16881947f45fb1c10ffb97d883e4ce34b786a
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-08-27 20:25:31 -0300

    styles: header style

--- react/src/layout/Header.tsx
+++ react/src/layout/Header.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "./styles/header.css"
 
 const Header: React.FC = () => {
     return (
-        <>
+        <header className="header">
             <Link to="/">Home</Link>
             <Link to="/basic">Basic</Link>
             <Link to="/grid">Grid</Link>
             <Link to="/flex">Flex</Link>
             <Link to="/todolist">Todo List</Link>
-        </>
+        </header>
     )
 }
 
---
+++ react/src/pages/About/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./about.css"
+
+const About: React.FC = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default About
