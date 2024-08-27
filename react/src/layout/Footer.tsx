0cb6b808367146b8cf36b07145c411657fae9f62
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-08-27 20:43:59 -0300

    feat: home section

--- react/src/layout/Footer.tsx
+++ react/src/layout/Footer.tsx
@@ -1,10 +1,34 @@
 import React from "react"
+import "./styles/footer.css"
+import { Link } from "react-router-dom"
 
 const Footer: React.FC = () => {
     return (
-        <>
-
-        </>
+        <footer className="footer">
+            <section className="sitemap">
+                <h3>Site Map</h3>
+                <ul>
+                    <Link to="/">
+                        <li>Home</li>
+                    </Link>
+                </ul>
+                <ul>
+                    <Link to="/basic">
+                        <li>Basic</li>
+                    </Link>
+                </ul>
+                <ul>
+                    <Link to="/grid">
+                        <li>Grid</li>
+                    </Link>
+                </ul>
+                <ul>
+                    <Link to="/todolist">
+                        <li>Todo List</li>
+                    </Link>
+                </ul>
+            </section>
+        </footer>
     )
 }
 
--- react/src/pages/Home/Home.tsx
+++ react/src/pages/Home/Home.tsx
@@ -3,8 +3,10 @@ import React from "react"
 
 const Home: React.FC = () => {
     return (
-        <>
-        </>
+        <main className="mainContainer">
+            <h2 className="title-message">Yes, bro. This is my home</h2>
+            <p className="text-message">This project is just for remember some concepts about react, html, css and js</p>
+        </main>
     )
 }
 
